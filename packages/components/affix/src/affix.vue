873f28a5742a17f2e17d26f45870bd49942d1840
ZRMYDYCG <547471919@qq.com>
2024-11-07 21:55:22 +0800

    feat: 初始化 affix(#5)

---
+++ packages/components/affix/src/affix.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineOptions({
+  name: 'YqAffix'
+})
+</script>
+
+<template>
+  <div class="yq-affix"></div>
+</template>
+
+<style scoped lang="scss"></style>
--- play/src/App.vue
+++ play/src/App.vue
@@ -1,44 +1,5 @@
-<script setup lang="ts">
-import YqContainer from '@yq-design/components/container/src/container.vue'
-import YqHeader from '@yq-design/components/container/src/header.vue'
-import YqFooter from '@yq-design/components/container/src/footer.vue'
-import YqMain from '@yq-design/components/container/src/main.vue'
-import YqAside from '@yq-design/components/container/src/aside.vue'
-import '@yq-design/theme-chalk/container.scss'
-import '@yq-design/theme-chalk/aside.scss'
-import '@yq-design/theme-chalk/header.scss'
-import '@yq-design/theme-chalk/main.scss'
-import '@yq-design/theme-chalk/footer.scss'
-</script>
+<script setup lang="ts"></script>
 
-<template>
-  <YqContainer>
-    <YqAside></YqAside>
-    <YqMain>
-      <YqHeader></YqHeader>
-      <YqMain></YqMain>
-      <YqFooter></YqFooter>
-    </YqMain>
-  </YqContainer>
-</template>
+<template></template>
 
-<style scoped lang="scss">
-.yq-container {
-  background-color: blueviolet;
-  height: 100vh;
-  .yq-aside {
-    height: 100%;
-    background-color: yellow;
-  }
-  .yq-main {
-    height: 100%;
-    background-color: aqua;
-    .yq-header {
-      background-color: black;
-    }
-    .yq-footer {
-      background-color: palegreen;
-    }
-  }
-}
-</style>
+<style scoped lang="scss"></style>
