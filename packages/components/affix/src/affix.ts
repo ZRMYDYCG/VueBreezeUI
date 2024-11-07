d80345c60876cd2646ac338b5f4ab6b2b3505017
ZRMYDYCG <547471919@qq.com>
2024-11-07 22:05:05 +0800

    feat: 基本类型(#5)

---
+++ packages/components/affix/src/affix.ts
@@ -0,0 +1,12 @@
+export type IPositionType = 'top' | 'bottom'
+
+export interface IAffixProps {
+  /*偏移的距离*/
+  offset?: number
+  /*固钉位置*/
+  position: IPositionType
+  /*指定容器(CSS选择器)*/
+  target?: string
+  /*z-index属性值*/
+  'z-index': number
+}
