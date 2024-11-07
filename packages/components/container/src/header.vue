debedbdf066a8fa713e5a106412ee3f13a87b455
ZRMYDYCG <547471919@qq.com>
2024-11-07 20:13:13 +0800

    feat: yq-header(#4)

--- packages/components/container/src/header.vue
+++ packages/components/container/src/header.vue
@@ -1,11 +1,30 @@
 <script setup lang="ts">
+import { computed } from 'vue'
+import type { CSSProperties } from 'vue'
+import type { IPropsHeader } from './layout'
+
 defineOptions({
   name: 'YqHeader'
 })
+
+const props = withDefaults(defineProps<IPropsHeader>(), {
+  height: '60px'
+})
+
+const style = computed(() => (props.height ? { height: props.height } : {}) as CSSProperties)
 </script>
 
 <template>
-  <div class="yq-header"></div>
+  <header class="yq-header" :style="style">
+    <slot></slot>
+  </header>
 </template>
 
-<style scoped></style>
+<style scoped>
+.yq-header {
+  padding: 20px;
+  box-sizing: border-box;
+  flex-shrink: 0;
+  height: 60px;
+}
+</style>
