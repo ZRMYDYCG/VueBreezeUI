4420ec776fb73676b19e8634b9cc36bb6694a85a
ZRMYDYCG <547471919@qq.com>
2024-11-10 22:57:51 +0800

    chore: update

--- packages/components/affix/src/affix.ts
+++ packages/components/affix/src/affix.ts
@@ -4,9 +4,9 @@ export interface IAffixProps {
   /*偏移的距离*/
   offset?: number
   /*固钉位置*/
-  position: IPositionType
+  position?: IPositionType
   /*指定容器(CSS选择器)*/
   target?: string
   /*z-index属性值*/
-  'z-index': number
+  'z-index'?: number
 }
