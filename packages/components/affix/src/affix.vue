e0382179c0e2851a22dc2c3119a8855ebf7c65d4
ZRMYDYCG <547471919@qq.com>
2024-11-11 18:51:41 +0800

    feat: Snap to Window

--- packages/components/affix/src/affix.vue
+++ packages/components/affix/src/affix.vue
@@ -1,13 +1,38 @@
+<!--
+ * @Author: ZRMYDYCG
+ * @Date: 2024-11
+ * @LastEditors: ZRMYDYCG
+ * @LastEditTime: 2024-11
+ * @Description: 
+-->
 <script setup lang="ts">
-import { ref, nextTick, watchEffect, watch, onMounted } from 'vue'
+import { ref, nextTick, watchEffect, watch, onMounted, computed } from 'vue'
+import type { CSSProperties } from 'vue'
 import type { IAffixProps } from './affix'
 import { useElementBounding, useWindowSize, useEventListener } from '@vueuse/core'
 
 const props = defineProps<IAffixProps>()
 const emits = defineEmits(['change', 'scroll'])
 
+const target = ref<HTMLElement>()
 const root = ref<HTMLDivElement>()
 const scrollContainer = ref<HTMLElement | Window>() // 滚动目标容器
+const rootStyle = computed<CSSProperties>(() => {
+  return {}
+})
+const targetStyle = computed<CSSProperties>(() => {
+  if (!fixed.value) return {}
+
+  const { offset } = props
+
+  return {
+    height: `${rootHeight.value}px`,
+    width: `${rootWidth.value}px`,
+    top: props.position === 'top' ? offset + 'px' : '',
+    bottom: props.position === 'bottom' ? offset + 'px' : '',
+    zIndex: props.zIndex
+  }
+})
 
 const fixed = ref(false)
 const scrollTop = ref(0)
@@ -15,7 +40,7 @@ const scrollTop = ref(0)
 const {
   height: rootHeight,
   width: rootWidth,
-  top: roothTop,
+  top: rootTop,
   bottom: rootBottom,
   // update: 用于更新元素的边界信息
   update: updateRoot
@@ -35,9 +60,18 @@ const update = () => {
     scrollContainer.value instanceof Window
       ? document.documentElement.scrollTop
       : scrollContainer.value.scrollTop || 0
-  const { position } = props
+  const { position, offset } = props
+
+  console.log('offset', offset)
+  console.log('scrollTop', scrollTop.value)
+  console.log('rootTop', rootTop.value)
+
   if (position === 'top') {
-    fixed.value = true
+    if (offset > rootTop.value) {
+      fixed.value = true
+    }
+  } else if (position === 'bottom') {
+    fixed.value = windowHeight.value - offset < rootBottom.value
   }
 }
 
@@ -45,7 +79,7 @@ nextTick(() => {
   console.log(
     rootHeight.value,
     rootWidth.value,
-    roothTop.value,
+    rootTop.value,
     rootBottom.value,
     windowHeight.value
   )
@@ -58,13 +92,14 @@ watchEffect(update)
 
 onMounted(() => {
   scrollContainer.value = window
+  target.value = document.documentElement
   updateRoot()
 })
 </script>
 
 <template>
-  <div ref="root" class="yq-affix">
-    <div :class="['yq-affix-target', { 'yq-affix--fixed': fixed }]">
+  <div ref="root" class="yq-affix" :style="rootStyle">
+    <div :class="['yq-affix-target', { 'yq-affix--fixed': fixed }]" :style="targetStyle">
       <slot></slot>
     </div>
   </div>
--- play/src/App.vue
+++ play/src/App.vue
@@ -10,11 +10,14 @@ import YqAffix from '@yq-design/components/affix/src/affix.vue'
 </script>
 
 <template>
-  <div class="h-[2000px]">
-    <YqAffix position="top">
+  <div class="h-[300px]"></div>
+  <div class="h-[2000px]"></div>
+  <div>
+    <YqAffix position="bottom" :offset="100">
       <button class="w-20 h-10 bg-orange-500 rounded-lg">Affix</button>
     </YqAffix>
   </div>
+  <div class="h-[3000px]"></div>
 </template>
 
 <style scoped lang="scss"></style>
