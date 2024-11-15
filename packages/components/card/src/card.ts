6a5828f409f6cf2b8ec9e4df006f4e86b70855dd
ZRMYDYCG <547471919@qq.com>
2024-11-15 17:42:04 +0800

    feat: card interface

---
+++ packages/components/card/src/card.ts
@@ -0,0 +1,22 @@
+import type { CSSProperties } from 'vue'
+
+export interface ICardProps {
+  /*卡片宽度*/
+  width?: number | string
+  /*卡片是否有边框*/
+  border?: boolean
+  /*卡片的尺寸*/
+  size?: 'small' | 'medium' | 'large'
+  /*鼠标经过, 卡片是否浮动*/
+  hoverSkip?: boolean
+  /*是否添加卡片内容正在加载中的一个状态*/
+  loading?: boolean
+  /*卡片标题*/
+  title?: string
+  /*卡片右上角的操作*/
+  extra?: string
+  /*标题区域自定义样式*/
+  headStyle?: CSSProperties
+  /*卡片内容区域自定义样式*/
+  bodyStyle?: CSSProperties
+}
--- packages/components/card/src/card.vue
+++ packages/components/card/src/card.vue
@@ -1,4 +1,28 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { ICardProps } from './card'
+import type { IAvatarProps } from '../../avatar/src/avatar'
+
+const props = withDefaults(defineProps<ICardProps>(), {
+  /*卡片宽度*/
+  width: 'auto',
+  /*卡片是否有边框*/
+  border: false,
+  /*卡片的尺寸*/
+  size: 'medium',
+  /*鼠标经过, 卡片是否浮动*/
+  hoverSkip: false,
+  /*是否添加卡片内容正在加载中的一个状态*/
+  loading: false,
+  /*卡片标题*/
+  title: undefined,
+  /*卡片右上角的操作*/
+  extra: undefined,
+  /*标题区域自定义样式*/
+  headStyle: undefined,
+  /*卡片内容区域自定义样式*/
+  bodyStyle: undefined
+})
+</script>
 
 <template>Card</template>
 
