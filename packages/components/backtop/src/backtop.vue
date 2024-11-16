ae7204d65b46a98b213121a408ebbb07dbf732b1
ZRMYDYCG <547471919@qq.com>
2024-11-16 15:05:45 +0800

    feat: Wrap the backTop with transition

--- packages/components/backtop/src/backtop.vue
+++ packages/components/backtop/src/backtop.vue
@@ -1,9 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+const visible = ref(false)
+</script>
 
 <template>
-  <div class="yq-back-top">
-    <slot></slot>
-  </div>
+  <button @click="visible = !visible">切换</button>
+  <Transition name="fade">
+    <div class="yq-back-top" v-if="!visible">
+      <slot></slot>
+    </div>
+  </Transition>
 </template>
 
 <style scoped></style>
--- play/src/views/backtop.vue
+++ play/src/views/backtop.vue
@@ -3,13 +3,9 @@ import BackTop from '@yq-design/components/backtop/src/backtop.vue'
 </script>
 
 <template>
-  <BackTop>
-    <div
-      class="w-[40px] h-[40px] rounded-full flex justify-center items-center bg-gray-500 text-white text-sm cursor-pointer hover:bg-gray-600"
-    >
-      <component :is="'Top'"></component>
-    </div>
-  </BackTop>
+  <div class="h-screen w-screen bg-blue-500">
+    <BackTop> 88 </BackTop>
+  </div>
 </template>
 
 <style scoped></style>
