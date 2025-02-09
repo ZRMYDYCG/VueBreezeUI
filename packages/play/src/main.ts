ec2ac4fe5f36d5a53725520b964e5497e6ca0877
ZRMYDYCG <547471919@qq.com>
2025-02-09 16:53:17 +0800

    feat: npm 包名冲突

--- packages/play/src/main.ts
+++ packages/play/src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import  BreezeUi from "breeze-ui"
-import "breeze-ui/dist/index.css"
+import  BreezeUi from "breeze-ui-lib"
+import "breeze-ui-lib/dist/index.css"
 
 createApp(App).use(BreezeUi).mount('#app')
