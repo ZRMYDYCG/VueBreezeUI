05d60e5f2248a6019967f3887571dd8aa906ea81
ZRMYDYCG <547471919@qq.com>
2025-03-11 16:39:31 +0800

    feat: ✨️完善抽屉四个方向的动画

--- packages/play/src/views/Drawer/index.vue
+++ packages/play/src/views/Drawer/index.vue
@@ -3,11 +3,14 @@ import { BreDrawer } from '../../../../components'
 import { ref } from 'vue'
 import { BreButton } from '../../../../components'
 
-const visible = ref(false)
+const visibleA = ref(false)
+const visibleB = ref(false)
+const visibleC = ref(false)
+const visibleD = ref(false)
 </script>
 
 <template>
-  <BreDrawer v-model="visible" title="设置面板" direction="rtl" :size="400">
+  <BreDrawer v-model="visibleA" title="设置面板A" direction="rtl" :size="400">
     <template #title>
       <h2>自定义标题</h2>
     </template>
@@ -18,11 +21,58 @@ const visible = ref(false)
     </form>
 
     <template #footer>
-      <BreButton @click="visible = false">关闭</BreButton>
+      <BreButton @click="visibleA = false">关闭</BreButton>
     </template>
   </BreDrawer>
+  <BreButton @click="visibleA = true">打开设置面板A</BreButton>
 
-  <BreButton @click="visible = true">打开设置面板</BreButton>
+  <BreDrawer v-model="visibleB" title="设置面板B" direction="ltr" :size="400">
+    <template #title>
+      <h2>自定义标题</h2>
+    </template>
+
+    <form class="settings-form">
+      <!-- 表单内容 -->
+      <input type="text" placeholder="请输入姓名" />
+    </form>
+
+    <template #footer>
+      <BreButton @click="visibleB = false">关闭</BreButton>
+    </template>
+  </BreDrawer>
+  <BreButton @click="visibleB = true">打开设置面板B</BreButton>
+
+  <BreDrawer v-model="visibleC" title="设置面板C" direction="ttb" :size="400">
+    <template #title>
+      <h2>自定义标题</h2>
+    </template>
+
+    <form class="settings-form">
+      <!-- 表单内容 -->
+      <input type="text" placeholder="请输入姓名" />
+    </form>
+
+    <template #footer>
+      <BreButton @click="visibleC = false">关闭</BreButton>
+    </template>
+  </BreDrawer>
+  <BreButton @click="visibleC = true">打开设置面板C</BreButton>
+
+  <BreDrawer v-model="visibleD" title="设置面板D" direction="btt" :size="400">
+    <template #title>
+      <h2>自定义标题</h2>
+    </template>
+
+    <form class="settings-form">
+      <!-- 表单内容 -->
+      <input type="text" placeholder="请输入姓名" />
+    </form>
+
+    <template #footer>
+      <BreButton @click="visibleD = false">关闭</BreButton>
+    </template>
+  </BreDrawer>
+  <BreButton @click="visibleD = true">打开设置面板D</BreButton>
 </template>
 
 <style scoped lang="scss"></style>
